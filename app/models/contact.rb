1350937185c18a7640727701d58865aa959b83db
Yashraj Jhala <jhalayashraj@gmail.com>
2017-12-07 21:14:01 +0530

    Initial commit

---
+++ app/models/contact.rb
@@ -0,0 +1,12 @@
+class Contact < ApplicationRecord
+  validates :name, presence: true, length: { maximum: 50 }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, length: { maximum: 255 },
+                    format: { with: VALID_EMAIL_REGEX },
+                    uniqueness: { case_sensitive: false }
+  validates :mobile, presence: true, length: { is: 10},
+                     uniqueness: true
+  scope :starts_with, ->(alphabet){ where("name like ?","#{alphabet}%") }
+
+  belongs_to :user
+end
